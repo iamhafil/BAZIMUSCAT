924a259e5ad480409ad707c1e6d6e71bfcb6b3df
Muhammed Hafil <hafizc007@gmail.com>
2018-08-28 22:38:22 +0530

    Initial commit

---
+++ api/app/Customer.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Authenticatable implements JWTSubject
+{
+    use Notifiable;
+    public $timestamps = false;
+    
+    public $fillable = ['customers_name','customers_email_address','customers_password'];
+     /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
